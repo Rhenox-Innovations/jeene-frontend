8caf426e4423b861c37415a3eebeae1027b93549
Sohaib Aijaz <sohaib.aijaz@menaa.com>
2024-12-18 17:38:30 +0400

    Add user image

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import SignInLayer from "../components/SignInLayer";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
 
 const SignInPage = () => {
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+  if(isAuthenticated) 
+    return <Navigate to="/dashboard" />;
   return (
     <>
 
